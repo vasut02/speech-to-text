d3f5a8a650677bb305ad5ef353835c8da21fb49c
vasut02 <vasutiwari409@gmail.com>
2024-07-14 21:33:04 +0530

    Made few changes

--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-# from routers import auth, deepgram_api
+from routers import auth, deepgram_api
 
 app = FastAPI()
 
-# app.include_router(auth.router)
-# app.include_router(deepgram_api.router)
+app.include_router(auth.router)
+app.include_router(deepgram_api.router)
 
 @app.get("/")
 def read_root():
